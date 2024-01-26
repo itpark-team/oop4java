2600d4f31dc8d421a5425f836e3e5d435efe0c6d
amsmyslov <a.m.smyslov@yandex.ru>
2024-01-26 20:12:52 +0300

    Initial commit

---
+++ src/SimpleMath.java
@@ -0,0 +1,9 @@
+public class SimpleMath {
+    public double getSum(double num1, double num2) {
+        return num1 + num2;
+    }
+
+    public double getMul(double num1, double num2) {
+        return num1 * num2;
+    }
+}
